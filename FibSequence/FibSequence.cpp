9c27410d408a834467fd03e10da4406d6c007935
Jigglebox <tylercchase@gmail.com>
2019-01-09 01:27:01 -0900

    push

--- FibSequence/FibSequence.cpp
+++ FibSequence/FibSequence.cpp
@@ -3,10 +3,30 @@
 
 #include "pch.h"
 #include <iostream>
-
+int fib(int power);
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::cout << "Please put a number to have the fibonacci sequence go to." << std::endl;
+	int input;
+	std::cin >> input;
+	if (input < 0) {
+		std::cout << "Please enter a positive number" << std::endl;
+		return 0;
+	}
+	std::cout << "Output is: " << fib(input) << std::endl;
+	std::cin;
+	return 0;
+}
+int fib(int power) {
+	if (power == 0) {
+		return 0;
+	}
+	else if (power == 1) {
+		return 1;
+	}
+	else {
+		return fib(power - 1) + fib(power - 2);
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
